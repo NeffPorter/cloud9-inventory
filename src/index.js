b57e14f7089f017879392c1499f12d50e7752e97
Neff <cloud9arizona@gmail.com>
2026-08-05 13:00:47 -0700

    Fix invoice upload: raise JSON body limit to 15mb (was 100kb default)

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ console.log('[Startup] GMAIL_PASS:', process.env.GMAIL_PASS ? '***set***' : 'MIS
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '15mb' }));
 
 // API Routes FIRST
 app.use('/api/auth', require('./routes/auth'));
